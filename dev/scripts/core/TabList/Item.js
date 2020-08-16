c4b55c2db133f7a5bed26a140f7b18f81555a41a
CMB <thaichibao@gmail.com>
2020-08-16 14:40:44 +0700

    fix menu not closing when clear itema or delete list, fix margin not update when clear items, press down when on title won't blur title, make item text looks better, item animation

--- dev/scripts/core/TabList/Item.js
+++ dev/scripts/core/TabList/Item.js
@@ -1,8 +1,6 @@
 import { createElement } from "../Utils.js";
 import { addFunctionalities, addEventListeners } from "./Item/Init.js";
 import { Type } from "../Type.js";
-import { TitleBox } from "./Item/TitleBox.js";
-import { insertElementBefore } from "../Utils/insertElementBefore.js";
 import { ContentBox } from "./Item/contentBox.js";
 import { DeleteButton } from "./Item/DeleteButton.js";
 import { CopyButton } from "./Item/CopyButton.js";
@@ -10,7 +8,6 @@ import { CopyButton } from "./Item/CopyButton.js";
 export class Item {
   static Create(tabList, orderNumber, textContent = "") {
     const item = createElement("div", "list__item");
-    insertElementBefore(tabList.getFutureItem(), item);
 
     item.draggable = true;
 
@@ -29,18 +26,17 @@ export class Item {
     item._deleteButton = DeleteButton.Create(item);
     item._copyButton = CopyButton.Create(item);
 
-    item.appendChild(item._order);
-    item.appendChild(item._contentBox);
-    item.appendChild(item._deleteButton);
-    item.appendChild(item._copyButton);
-
+    assembleComponents(item);
     addFunctionalities(item);
     addEventListeners(item);
 
-    // THIS ONLY WORKS ONLINE SO... WASTED MY TIME FOR NOTHING.
-    item._titleBox = TitleBox.Create(item);
-    item._titleBox.classList.add("--collapse");
-
     return item;
   }
+}
+
+function assembleComponents(item) {
+  item.appendChild(item._order);
+  item.appendChild(item._contentBox);
+  item.appendChild(item._deleteButton);
+  item.appendChild(item._copyButton);
 }
--- dev/scripts/core/TabList/MinimizeButton/Init/addEventListeners.js
+++ dev/scripts/core/TabList/MinimizeButton/Init/addEventListeners.js
@@ -1,9 +1,7 @@
-import { background } from "../../../../main.js";
 
 export function addEventListeners(minimizeButton) {
   minimizeButton.addEventListener("click", () => {
     minimizeButton.toggleMinimization();
-    background.updatePosition();
   });
 
   minimizeButton.addEventListener("mouseover", () =>
